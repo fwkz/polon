5efad6e8e351ed6b5f3eee73145b3a585d5a1f65
fwkz <f4wkes@gmail.com>
2014-09-24 13:55:11 +0200

    Docstrings update.

--- polon/core/handlers/loader.py
+++ polon/core/handlers/loader.py
@@ -4,10 +4,16 @@ from inspect import getmembers, isclass
 from polon.core.handlers import PageHandler
 
 
-current_test_handlers_module_path = None
+current_test_handlers_module_path = None  # Set before each test by Polon nose plugin at runtime.
 
 
 def load_handlers():
+    """ Handler loader.
+
+    Load handlers specific to currently executed test based on current_test_handlers_module_path attribute that is set
+    before each test by Polon nose plugin at runtime. Set back to None after test is done.
+    :return: Returns set of handlers specific to currently executed test.
+    """
     if not current_test_handlers_module_path:
         raise RuntimeError("Test handler path is not set. No test is being run.")
 
--- polon/plugins/__init__.py
+++ polon/plugins/__init__.py
@@ -3,15 +3,15 @@ from polon.core.handlers import loader
 
 
 class CurrentTestHandlers(Plugin):
+    """ Polon handlers plugin for nose.
+
+    Sets at runtime handlers path of currently executed test as
+    polon.core.handlers.loader.current_test_handlers_module_path attribute.
+    Set attribute back to None when test is done.
+    """
 
     name = "polon-handlers-plugin"
     score = 1
-
-    def options(self, parser, env):
-        super(CurrentTestHandlers, self).options(parser, env)
-
-    def configure(self, options, conf):
-        super(CurrentTestHandlers, self).configure(options, conf)
 
     def beforeTest(self, test):
         test_package_path, _, _ = test.address()[1].rpartition(".")
