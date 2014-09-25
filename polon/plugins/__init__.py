05679a61ea525e6357b2eb3946cb35df39cff050
Mariusz Kupidura <f4wkes@gmail.com>
2014-09-25 20:44:41 +0200

    Refactoring of CurrentTestHandlers nose plugin.

--- polon/plugins/__init__.py
+++ polon/plugins/__init__.py
@@ -7,12 +7,6 @@ class CurrentTestHandlers(Plugin):
     name = "polon-handlers-plugin"
     score = 1
 
-    def options(self, parser, env):
-        super(CurrentTestHandlers, self).options(parser, env)
-
-    def configure(self, options, conf):
-        super(CurrentTestHandlers, self).configure(options, conf)
-
     def beforeTest(self, test):
         test_package_path, _, _ = test.address()[1].rpartition(".")
         loader.current_test_handlers_module_path = "{}.handlers".format(test_package_path)
