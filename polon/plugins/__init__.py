f27a417bdd766f1319d3dd7899f527039f842933
fwkz <f4wkes@gmail.com>
2014-09-26 13:16:40 +0200

    Refactoring of nose plugin. Extending functionality. Plugin sets attributes for config and handlers.

--- polon/plugins/__init__.py
+++ polon/plugins/__init__.py
@@ -1,21 +1,28 @@
+import os
+
 from nose.plugins import Plugin
 from polon.core.handlers import loader
+from polon import scenarios
 
 
-class CurrentTestHandlers(Plugin):
-    """ Polon handlers plugin for nose.
+class PolonInterceptor(Plugin):
+    """ Polon interceptor plugin for nose.
 
-    Sets at runtime handlers path of currently executed test as
-    polon.core.handlers.loader.current_test_handlers_module_path attribute.
-    Set attribute back to None when test is done.
+    Sets following attributes at runtime based on currently executed test:
+        polon.core.handlers.loader.current_test_handlers_module_path attribute,
+        polon.scenarios.current_test_config_path attribute
+
+    Set attributes back to None when test is done.
     """
 
-    name = "polon-handlers-plugin"
+    name = "polon-interceptor"
     score = 1
 
     def beforeTest(self, test):
         test_package_path, _, _ = test.address()[1].rpartition(".")
         loader.current_test_handlers_module_path = "{}.handlers".format(test_package_path)
+        scenarios.current_test_config_path = os.path.join(os.path.dirname(test.address()[0]), "config.cfg")
 
     def afterTest(self, test):
         loader.current_test_handlers_module_path = None
+        scenarios.current_test_config_path = None
