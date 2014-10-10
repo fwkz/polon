61dd06b813ba469a6a97b4bf91f55b31f38f0933
fwkz <f4wkes@gmail.com>
2014-10-10 14:09:48 +0200

    Added StashClient

--- polon/stash/client.py
+++ polon/stash/client.py
@@ -3,11 +3,32 @@ import socket
 HOST = 'localhost'
 PORT = 50505
 
-while 1:
-    input_stream = raw_input(">> ")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    s.sendall(input_stream)
-    data = s.recv(1024)
-    s.close()
-    print '>> ', repr(data)
+
+def repl():
+    while 1:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        input_stream = raw_input(">> ")
+        s.sendall(input_stream)
+        data = s.recv(1024)
+        print '>> ', repr(data)
+        s.close()
+
+
+class StashClient(object):
+    def get(self, key):
+        return self.__execute("GET", key=key)
+
+    def put(self, key, value, value_type="STR"):
+        return self.__execute("PUT", key=key, value=value, value_type=value_type)
+
+    def __execute(self, method, key, value='', value_type=''):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        s.sendall(";".join([method, key, value, value_type]))
+        data = self.s.recv(1024)
+        self.s.close()
+        return data
+
+if __name__ == "__main__":
+    repl()
