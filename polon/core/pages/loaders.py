5bc3b589e7d3c691d475a2565af4e4a9179152e3
fwkz <f4wkes@gmail.com>
2014-12-04 15:17:43 +0100

    Reusable page objects loaders.

--- polon/core/pages/loaders.py
+++ polon/core/pages/loaders.py
@@ -2,18 +2,21 @@ import importlib
 from inspect import getmembers, isclass
 import pkgutil
 
-from polon.conf import settings
 from polon.core.exceptions import ImproperlyConfigured
 
 
-def load_pages_from_module(path):
+def load_pages_from_module(path, base_class_path=None):
     """ Page objects loader function.
 
     Retrieve pages from given module.
     :param path: Module dotted path.
     :return: Set of retrieved pages.
     """
-    base_class_module, base_class = settings.PAGE_BASE_CLASS.rsplit(".", 1)
+    if not base_class_path:
+        from polon.conf import settings
+        base_class_path = settings.PAGE_BASE_CLASS.rsplit(".", 1)
+
+    base_class_module, base_class = base_class_path.rsplit(".", 1)
     base_class = getattr(importlib.import_module(base_class_module), base_class)
 
     po_module = importlib.import_module(path)
@@ -22,7 +25,7 @@ def load_pages_from_module(path):
     return pages_set
 
 
-def load_pages_from_package(page_object_dirs):
+def load_pages_from_package(page_object_dirs, base_class_path=None):
     """ Page objects loader function.
 
     Retrieve pages from given package.
@@ -35,9 +38,8 @@ def load_pages_from_package(page_object_dirs):
         modules = [package.__name__] + [name for _, name, _ in pkgutil.walk_packages(package.__path__,
                                                                                      package.__name__ + '.')]
         for module_path in modules:
-            pages_set.update(load_pages_from_module(module_path))
+            pages_set.update(load_pages_from_module(module_path, base_class_path))
     return pages_set
-
 
 def load_pages():
     """ Retrieve page objects from all over the project.
@@ -45,6 +47,8 @@ def load_pages():
     Iterate over dictionary of loader functions, executes them and collect what is returned.
     :return: Set of retrieved pages that loader functions returns.
     """
+    from polon.conf import settings
+
     pages = set()
     page_loaders = {"PAGE_OBJECTS_DIRS": load_pages_from_package,
                     "PAGE_OBJECTS_MODULE": load_pages_from_module}
